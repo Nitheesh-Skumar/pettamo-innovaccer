19e101702e1ad2ae9e6b39dea001ba62956c1f49
Nitheeshskumar <nitheesh.s.k1710@gmail.com>
2022-04-09 23:04:30 +0530

    packgaes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import 'bootstrap/dist/css/bootstrap.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import './Assets/Main.css'
+import './Assets/Main.scss'
+// import 'bulma/css/bulma.css';
+
+// import "todomvc-app-css/index.css";
+import RootRoutes from "./Routes/Routes";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RootRoutes />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
